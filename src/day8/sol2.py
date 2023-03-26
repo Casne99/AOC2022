a7d1b5f992c7fbef30acd68a46dd145ba5760c74
Casne99 <luca.casnedi@protonmail.com>
2023-03-27 00:15:36 +0200

    Aggiunta soluzione giorno 8 parte 2

--- src/day8/sol2.py
+++ src/day8/sol2.py
@@ -11,47 +11,42 @@ class TreeField:
     def height(self, x, y):
         return self.grid[(x, y)]
 
-    def is_on_border(self, x, y):
-        return x == 0 or y == 0 or x == self.xlim or y == self.ylim
-
-    def is_visible_from(self, cx, cy, pos):
-        height = self.grid[(cx, cy)]
-        if pos == "TOP":
-            return all(self.grid[(cx, y)] < height for y in range(cy + 1, self.ylim + 1))
-        elif pos == "BOTTOM":
-            return all(self.grid[(cx, y)] < height for y in range(0, cy))
-        elif pos == "RIGHT":
-            return all(self.grid[(x, cy)] < height for x in range(cx + 1, self.xlim + 1))
-        elif pos == "LEFT":
-            return all(self.grid[(x, cy)] < height for x in range(0, cx))
-
-    def is_visible(self, x, y):
-        return self.is_on_border(x, y) or any(self.is_visible_from(x, y, pos) for pos in ["TOP", "BOTTOM", "RIGHT", "LEFT"])
-
-    def count_visible(self):
-        return sum(1 for (x, y) in self.grid.keys() if self.is_visible(x, y))
-
-    def visible_trees(self, x, y, to):
-        ty = res = 0
+    def list_from_to(self, x, y, to):
         if to == "TOP":
-            while ty < self.ylim + 1:
-
-        return 0
+            return list(self.height(x, p) for p in range(y, self.ylim + 1))
+        if to == "RIGHT":
+            return list(self.height(p, y) for p in range(x, self.xlim + 1))
+        if to == "LEFT":
+            return list(self.height(p, y) for p in range(x, -1, -1))
+        if to == "BOTTOM":
+            return list(self.height(x, p) for p in range(y, -1, -1))
 
     def scenic_score(self, x, y):
-        scores = []
-
+        lists = (self.list_from_to(x, y, dir)
+                 for dir in ["TOP", "LEFT", "RIGHT", "BOTTOM"])
+        scores = (visible(p) for p in lists)
         return math.prod(scores)
-
-    def __str__(self):
-        return str(self.grid)
 
 
 def initialize_grid(grid):
     res = {}
-    for x, line in enumerate(reversed(grid)):
-        for y, height in enumerate(line.strip()):
+    for y, line in enumerate(reversed(grid)):
+        for x, height in enumerate(line.strip()):
             res[(x, y)] = int(height)
+    return res
+
+
+def visible(treeList):
+    res = 0
+    start = treeList[0]
+    if len(treeList) == 1:
+        return 0
+    prev = start
+    for elem in treeList[1:]:
+        if elem >= start:
+            return res + 1
+        else:
+            res += 1
     return res
 
 
@@ -60,5 +55,6 @@ puzzle = initialize_grid(input)
 x_max = max(x for (x, _) in puzzle.keys())
 y_max = max(y for (_, y) in puzzle.keys())
 field = TreeField(x_max, y_max, puzzle)
-ans = field.count_visible()
+ans = max(field.scenic_score(x, y) for x in range(0, field.xlim + 1)
+          for y in range(0, field.ylim + 1))
 print(ans)
